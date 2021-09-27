7c5a7273aa0d3b3d8eec35c6e44e18450b422764
Lucas Coronel <lccoronel7@icloud.com>
2021-09-27 19:26:01 -0300

    ✨ feat: routes

---
+++ src/screen/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, TextTitle } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <TextTitle>Welcome to our caker lab !</TextTitle>
+    </Container>
+  );
+};
+
+export default Login;
