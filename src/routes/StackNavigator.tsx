9fcb1384d24d019fc804ec5ea37a3be15210d0fc
Lucas Coronel <lccoronel7@icloud.com>
2021-09-27 21:27:43 -0300

    ✨ feat: home routes

--- src/routes/StackNavigator.tsx
+++ src/routes/StackNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from '../screen/Login';
+import Home from '../screen/Home';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ const StackNavigator: React.FC = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="login" component={Login} />
+      <Screen name="home" component={Home} />
     </Navigator>
   );
 };
---
+++ src/screen/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import HomeTemplate from '../../components/templates/HomeTemplate';
+
+const Home: React.FC = () => {
+  return <HomeTemplate />;
+};
+
+export default Home;
