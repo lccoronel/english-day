eb315a61feb5a0a6e112a964feda4beb20c87b98
Lucas Coronel <lccoronel7@icloud.com>
2021-09-27 21:14:08 -0300

    ✨ feat: login template

--- src/screen/Login/index.tsx
+++ src/screen/Login/index.tsx
@@ -1,46 +1,9 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
-import { useTheme } from 'styled-components';
 
-import AtomDescriptionText from '../../components/atoms/AtomDescriptionText';
-import googleImg from '../../assets/google.png';
-import cklImg from '../../assets/ckl.png';
-import {
-  Container,
-  TextTitle,
-  ButtonSocialLogin,
-  ImageGoogle,
-  ImageCKL,
-} from './styles';
+import LoginTemplate from '../../components/templates/LoginTemplate';
 
 const Login: React.FC = () => {
-  const { colors } = useTheme();
-
-  const { width } = Dimensions.get('screen');
-
-  return (
-    <Container>
-      <TextTitle>
-        Welcome{'\n'}to our{'\n'}caker lab !
-      </TextTitle>
-
-      <AtomDescriptionText
-        color={colors.gray_500}
-        style={{ marginTop: width / 3 }}
-      >
-        Let`s start with login:
-      </AtomDescriptionText>
-
-      <ButtonSocialLogin>
-        <ImageGoogle source={googleImg} />
-        <AtomDescriptionText color={colors.gray_500}>
-          CONTINUE WITH GOOGLE
-        </AtomDescriptionText>
-      </ButtonSocialLogin>
-
-      <ImageCKL source={cklImg} />
-    </Container>
-  );
+  return <LoginTemplate />;
 };
 
 export default Login;
